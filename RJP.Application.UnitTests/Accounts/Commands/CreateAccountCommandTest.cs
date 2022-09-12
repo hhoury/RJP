87faecb0a0d38a762751996761e0920f9eda7267
hhoury <hhoury@hotmail.com>
2022-09-12 14:02:19 +0300

    create CreateAccountCommandTest

---
+++ RJP.Application.UnitTests/Accounts/Commands/CreateAccountCommandTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RJP.Application.DTOs.CreateAccountDto;
+
+namespace RJP.Application.UnitTests.Accounts.Commands
+{
+    public class CreateAccountCommandTest
+    {
+		private readonly IMapper _mapper;
+		private readonly Mock<IUnitOfWork> _mockUow;
+		private readonly CreateAccountDto _createAccountDto;
+
+		public CreateAccountCommandTest()
+		{
+			_mockUow = MockUnitOfWork.GetUnitOfWork();
+		}
+	}
+}
